3f845ae71e84589a64566d27cc99ab161dce4055
start.vaadin.com <no-reply@vaadin.com>
2025-10-30 07:15:58 +0000

    Generated project

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+
+@SpringBootApplication
+@StyleSheet("styles.css")
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
